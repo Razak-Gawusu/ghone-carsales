153e5989f584cded7d56edd62e036c50c41e7541
Razak-Gawusu <gawusur@gmail.com>
2022-07-08 01:21:03 +0100

    finished with hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import Header from '../components/Header'
-import { Outlet } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <>
+    <section className='hero container'>
+      <div className="hero__text">
+        <h1 className="mainTitle">Looking to buy a quality used vehicle?</h1>
+        <img src="./images/car.webp" alt="car" className='hero__text--image' />
+        <p className='paragraph'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla ducimus error consequatur cumque expedita, illum facilis ipsam quas velit.
+        </p>
+        <div className='hero__links'>
+         <Link to='/inventory' className='btn'> view inventory</Link> 
+         <Link to='/contact' className='btn callToAction'> get in touch</Link> 
+        </div>
+      </div>
+
+      <img className='hero__image' src="./images/car.webp" alt="car" />
+    </section>
+    </>
   )
 }
 
